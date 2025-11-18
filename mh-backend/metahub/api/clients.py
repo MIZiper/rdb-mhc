06983e1cda42df1c8325d904e9eef7e2e8c1bb2e
MIZiper <miziper@163.com>
2025-11-18 22:07:45 +0100

    tiny tiny routers

--- mh-backend/metahub/api/clients.py
+++ mh-backend/metahub/api/clients.py
@@ -1,4 +1,18 @@
 from fastapi import APIRouter, Depends
+from asyncpg.connection import Connection
+from metahub.models.clients import Category, Client
 from metahub.db.connection import get_db
 
 router = APIRouter()
+
+async def register_client(client: Client, conn: Connection=Depends(get_db)):
+    ...
+
+async def get_clients(conn: Connection=Depends(get_db)):
+    ...
+
+async def register_category(client: Client, conn: Connection=Depends(get_db)):
+    ...
+
+async def get_categories_of(client: Client, conn: Connection=Depends(get_db)):
+    ...
--- mh-backend/metahub/api/tags.py
+++ mh-backend/metahub/api/tags.py
@@ -1,8 +1,16 @@
 from fastapi import APIRouter, Depends
+from asyncpg.connection import Connection
 from metahub.db.connection import get_db
+from metahub.models.tags import Tag
+from metahub.models.clients import Category
 
 router = APIRouter()
 
-@router.get("/tags")
-async def get_tags():
+async def get_tags_of(category: Category, conn: Connection=Depends(get_db)):
     pass
+
+async def add_tag_for(category: Category, conn: Connection=Depends(get_db)):
+    ...
+
+async def modify_tag(tag: Tag, conn: Connection=Depends(get_db)):
+    ...
