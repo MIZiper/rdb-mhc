f7b4de78602d44f2c1fdf3363759bdd29fc7fbe7
MIZiper <miziper@163.com>
2025-11-24 21:28:45 +0100

    tags frontend

---
+++ mh-frontend/src/lib/TagsGraph.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import type { Tag } from '../schema';
+    let canvas: HTMLCanvasElement;
+    let { tags, selectedTag } : { tags: Tag[], selectedTag: Tag | null} = $props();
+</script>
+
+<div class="tags-graph">
+    <canvas bind:this={canvas}></canvas>
+</div>
---
+++ mh-frontend/src/lib/TagsSelector.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import { onMount } from 'svelte';
+    import type { Tag } from '../schema';
+    
+    export let width: number = 600;
+    export let height: number = 400;
+    
+    let tags: Tag[] = $state([]);
+    
+    onMount(async () => {
+        tags = await fetchTagsGraph();
+    });
+</script>
