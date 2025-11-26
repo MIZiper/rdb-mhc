4c6a07b337dfe57054edc2cfefd29b21aecc9236
MIZiper <miziper@163.com>
2025-11-26 21:51:59 +0100

    use sveltestrap for frontend

--- mh-frontend/src/App.svelte
+++ mh-frontend/src/App.svelte
@@ -1,31 +1,52 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import { Styles } from "@sveltestrap/sveltestrap";
+  import {
+    Navbar,
+    NavbarBrand,
+    Collapse,
+    Nav,
+    NavItem,
+    NavLink,
+    NavbarToggler,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem,
+  } from "@sveltestrap/sveltestrap";
+  let isOpen: boolean = $state(false);
 </script>
 
+<Styles />
+
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <Navbar>
+    <NavbarBrand href="/">MetaHub</NavbarBrand>
 
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+    <Nav>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <Dropdown nav {isOpen} toggle={() => (isOpen = !isOpen)}>
+        <DropdownToggle nav caret>Dropdown</DropdownToggle>
+        <DropdownMenu>
+          <DropdownItem header>Header</DropdownItem>
+          <DropdownItem disabled>Action</DropdownItem>
+          <DropdownItem>Another Action</DropdownItem>
+          <DropdownItem divider />
+          <DropdownItem>Another Action</DropdownItem>
+        </DropdownMenu>
+      </Dropdown>
+      <NavItem>
+        <NavLink href="#">Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink href="#">Another Link</NavLink>
+      </NavItem>
+      <NavItem>
+        <NavLink disabled href="#">Disabled Link</NavLink>
+      </NavItem>
+    </Nav>
+  </Navbar>
 </main>
 
 <style>
