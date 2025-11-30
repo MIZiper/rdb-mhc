3b89de685100ca3a722e8d6e0a623f7c5f8ba9fc
MIZiper <miziper@163.com>
2025-11-30 21:30:12 +0100

    use ENV for db connection

--- mh-backend/metahub/db/connection.py
+++ mh-backend/metahub/db/connection.py
@@ -1,16 +1,25 @@
+import os
+
 import asyncpg
 from typing import AsyncGenerator
 
 pool = None
 
+DB_USER = os.getenv("POSTGRES_USER", "postgres")
+DB_PASSWORD = os.getenv("POSTGRES_PASSWORD", "password")
+DB_NAME = os.getenv("POSTGRES_DB", "metahub")
+DB_HOST = os.getenv("POSTGRES_HOST", "localhost")
+DB_PORT = int(os.getenv("POSTGRES_PORT", "5432"))
+
 async def init_pool():
     global pool
 
     pool = await asyncpg.create_pool(
-        user="postgres",
-        password="password",
-        database="metahub",
-        host="localhost",
+        user=DB_USER,
+        password=DB_PASSWORD,
+        database=DB_NAME,
+        host=DB_HOST,
+        port=DB_PORT,
     )
 
 async def close_pool():
