9b92f4e3cdad4a1a91397302152dbaab0d8b0b8b
MIZiper <miziper@163.com>
2026-01-08 23:40:50 +0100

    make proxy server port configurable

--- mh-frontend/vite.config.ts
+++ mh-frontend/vite.config.ts
@@ -1,5 +1,11 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+
+const env = loadEnv("all", false);
+const apiHost = env.VITE_API_HOST || 'localhost';
+const apiPort = env.VITE_API_PORT || '8000';
+const apiTarget = `http://${apiHost}:${apiPort}`;
+
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -7,7 +13,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:8000",
+        target: apiTarget,
         changeOrigin: true,
       }
     }
